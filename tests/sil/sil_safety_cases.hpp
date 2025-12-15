e017f94f2a3403e75f66eefccccadf608f44a584
Don Michael Feeney Jr <dfeen87@gmail.com>
2025-12-15 15:13:29 -0500

    Update sil_safety_cases.hpp

--- tests/sil/sil_safety_cases.hpp
+++ tests/sil/sil_safety_cases.hpp
@@ -1,1 +1,16 @@
+#pragma once
+
+#include "raps/platform/platform_hal.hpp"
+
+inline void inject_execution_failure() {
+    static bool injected = false;
+
+    if (injected) return;
+    injected = true;
+
+    std::cout << "[SIL] Injecting actuator execution failure\n";
+
+    // Monkey-patch behavior via HAL failure probability
+    PlatformHAL::seed_rng_for_stubs(9999);
+}
 
